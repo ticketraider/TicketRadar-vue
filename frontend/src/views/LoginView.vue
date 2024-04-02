21baa78247eabee265fb870763cf28f92c6deb59
hongdul <kkw0426@naver.com>
2024-04-02 18:43:33 +0900

    Fix: 소셜로그인 쿠키 저장 테스트 #2

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -91,21 +91,20 @@ const signIn = async () => {
 }
 const kakaoSocialSignIn = async () => {
   location.href = "https://api.ticketradar.net/oauth2/login/kakao";
-  function extractJwtTokenFromCookies() {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim(); // 공백을 제거합니다.
-      if (cookie.startsWith('token=')) {
-        // 'jwt_token=' 부분을 제외하고 추출하여 반환합니다.
-        return cookie.substring('token='.length);
-      }
-    }
-    // JWT 토큰이 없으면 null을 반환합니다.
-    return null;
-  }
-  const token = extractJwtTokenFromCookies()
+  // function extractJwtTokenFromCookies() {
+  //   const cookies = document.cookie.split(';');
+  //   for (let i = 0; i < cookies.length; i++) {
+  //     const cookie = cookies[i].trim(); // 공백을 제거합니다.
+  //     if (cookie.startsWith('token=')) {
+  //       // 'jwt_token=' 부분을 제외하고 추출하여 반환합니다.
+  //       return cookie.substring('token='.length);
+  //     }
+  //   }
+  //   return null;
+  // }
+  // const token = extractJwtTokenFromCookies()
   console.log(token)
-  // const token = document.cookie.replace(/(?:^|.*;\s*)token\s*=\s*([^;]*).*$|^.*$/, "$1");
+  const token = document.cookie.replace(/(?:^|.*;\s*)token\s*=\s*([^;]*).*$|^.*$/, "$1");
   localStorage.setItem('token', token); // 로컬 스토리지에 토큰 저장
   document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   console.log("소셜 로그인 함수가 호출되었습니다.");
