ac9d9cfe1df5d76d4c99866e5a15b192489897a8
hongdul <kkw0426@naver.com>
2024-04-03 01:13:01 +0900

    소셜로그인 쿠키 저장 테스트 #2

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -63,6 +63,7 @@ function register() {
 function logout() {
   // 로그아웃 로직 실행, 예: 토큰 삭제
   localStorage.removeItem('token');
+  document.cookie = `token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
   location.reload(); // 간단하게 페이지를 새로고침하여 상태를 초기화합니다.
 }
 
