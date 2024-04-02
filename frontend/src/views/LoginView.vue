88b9f5499c2c7647210fe4c71bd6aafa10b2b3ba
hongdul <kkw0426@naver.com>
2024-04-02 15:58:48 +0900

    Fix: 소셜로그인 쿠키 저장 테스트 #2

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -90,14 +90,16 @@ const signIn = async () => {
   }
 }
 const kakaoSocialSignIn = async () => {
-  window.open("https://api.ticketradar.net/oauth2/login/kakao", '', 'width=400,height=600');
+  const loginWindow = window.open("https://api.ticketradar.net/oauth2/login/kakao", '', 'width=400,height=600');
+  loginWindow.close()
   const token = document.cookie.replace(/(?:^|.*;\s*)token\s*=\s*([^;]*).*$|^.*$/, "$1");
   localStorage.setItem('token', token); // 로컬 스토리지에 토큰 저장
   document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   console.log("소셜 로그인 함수가 호출되었습니다.");
 }
 const googleSocialSignIn = async () => {
-  window.open("https://api.ticketradar.net/oauth2/login/google", '', 'width=400,height=600')
+  const loginWindow = window.open("https://api.ticketradar.net/oauth2/login/google", '', 'width=400,height=600')
+  loginWindow.close()
   const token = document.cookie.replace(/(?:^|.*;\s*)token\s*=\s*([^;]*).*$|^.*$/, "$1");
   localStorage.setItem('token', token); // 로컬 스토리지에 토큰 저장
   document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
