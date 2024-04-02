6d9c5a8fe86c5487d440b6f7f6fdd9e62258bbf8
hongdul <kkw0426@naver.com>
2024-04-03 06:52:53 +0900

    소셜로그인 쿠키 저장 테스트 #2

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -2,7 +2,6 @@
 import {computed, ref} from 'vue';
 import { useRouter } from 'vue-router';
 import {jwtDecode} from "jwt-decode";
-import axios from "axios";
 
 const router = useRouter();
 const searchText = ref(''); // 검색어
