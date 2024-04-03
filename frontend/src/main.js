e49d4bd23618d196ed78d691f64cff92eb13338c
hongdul <kkw0426@naver.com>
2024-04-03 09:42:58 +0900

    vuecookie 에러 제거

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,8 +10,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import {loadFonts} from './plugins/webfontloader'
-import vueCookies from "vue-cookies";
-
 
 loadFonts()
 
